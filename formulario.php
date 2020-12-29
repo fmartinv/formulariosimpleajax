20ff911ea50e54725cf3bf923d959d3f02c7fb58
Fernando <villafae>
2020-12-28 22:18:04 -0300

    cambios de ajax

--- formulario.php
+++ formulario.php
@@ -3,35 +3,35 @@
 $error = '';
 
 //VALIDANDO NOMBRE
-if(empty($_POST["nombre"])){
+if (empty($_POST["nombre"])) {
     $error = 'Ingresa un nombre </br>';
-}else{
+} else {
     $nombre = $_POST["nombre"];
     $nombre = filter_var($nombre, FILTER_SANITIZE_STRING);
     $nombre = trim($nombre);
-    if($nombre==''){
+    if ($nombre == '') {
         $error .= 'Nombre está vacio</br>';
     }
 }
 //VALIDANDO E-MAIL
-if(empty($_POST["email"])){
+if (empty($_POST["email"])) {
     $error .= 'Ingresa un E-mail</br>';
-}else{
+} else {
     $email = $_POST["email"];
-    if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         $error .= 'Ingresa un E-mail verdadero</br>';
-    }else{
-        $email = filter_var($email,FILTER_SANITIZE_EMAIL);
+    } else {
+        $email = filter_var($email, FILTER_SANITIZE_EMAIL);
     }
 }
 //VALIDANDO MENSAJE
-if(empty($_POST["mensaje"])){
+if (empty($_POST["mensaje"])) {
     $error .= 'Ingresa un mensaje </br>';
-}else{
+} else {
     $mensaje = $_POST["mensaje"];
     $mensaje = filter_var($mensaje, FILTER_SANITIZE_STRING);
     $mensaje = trim($mensaje);
-    if($mensaje==''){
+    if ($mensaje == '') {
         $error .= 'Mensaje está vacio</br>';
     }
 }
@@ -40,11 +40,11 @@ if(empty($_POST["mensaje"])){
 $cuerpo .= "Nombre: ";
 $cuerpo .= $nombre;
 $cuerpo .= "\n";
- 
+
 $cuerpo .= "Email: ";
 $cuerpo .= $email;
 $cuerpo .= "\n";
- 
+
 $cuerpo .= "Mensaje: ";
 $cuerpo .= $mensaje;
 $cuerpo .= "\n";
@@ -54,9 +54,9 @@ $enviarA = 'fmartinvillaf@gmail.com'; //REEMPLAZAR CON TU CORREO ELECTRÓNICO
 $asunto = 'Nuevo mensaje de mi sitio web';
 
 //ENVIAR CORREO
-if($error == ''){
-    $success = mail($enviarA,$asunto,$cuerpo,'de: '.$email);
-    echo 'exito';
-}else{
+if ($error == '') {
+    $success = mail($enviarA, $asunto, $cuerpo, 'de: ' . $email);
+    echo "exito";
+} else {
     echo $error;
 }
